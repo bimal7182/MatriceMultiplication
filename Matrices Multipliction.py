2155dfee6e463e82e83960a60609514813eca15e
bimalnaik <bimal7182@gmail.com>
2020-07-02 20:49:09 +0200

    Numpy usage

--- Matrices Multipliction.py
+++ Matrices Multipliction.py
@@ -1,7 +1,9 @@
+import numpy as np
+import matplotlib as mpl
+
 matrixA = [[1, 4, 3],
            [5, 7, 9],
            [2, 7, 8]]
-
 matrixB = [[4, 3, 2],
            [7, 1, 5],
            [2, 9, 1]]
@@ -13,7 +15,7 @@ result = [[0, 0, 0],
 zipMatrix = zip(matrixA, matrixB)
 tupleZipMatrix = tuple(zipMatrix)
 
-print(tupleZipMatrix[0][0][2])
+#print(tupleZipMatrix[0][0][2])
 
 columnLenA = int(len(matrixA))
 rowLenA = int(len(matrixA[0]))
@@ -23,38 +25,66 @@ rowLenB = int(len(matrixB[0]))
 
 
 def dotProduct(sampleMatrix1, sampleMatrix2):
-    for i in range(rowLenA):
-        for j in range(columnLenB):
-            for k in range(rowLenA):
-                result[i][j] += sampleMatrix1[i][k] * sampleMatrix2[k][j]
-    print(result)
+    # for i in range(rowLenA):
+    #     for j in range(columnLenB):
+    #         for k in range(rowLenA):
+    #             result[i][j] += sampleMatrix1[i][k] * sampleMatrix2[k][j]
+    # print(result)
+    # x= np.array(sampleMatrix1)
+    # y= np.array(sampleMatrix2)
+    z = np.dot(sampleMatrix1, sampleMatrix2)
+    # np.reshape(z,(3,3))
+    print(z)
 
 
 def columnProduct(sampleMatrix1, sampleMatrix2):
-    for i in range(columnLenB):
-        for j in range(columnLenA):
-            for k in range(columnLenB):
-                result[i][j] += sampleMatrix2[k][i] * sampleMatrix1[k][j]
+    # for i in range(rowLenA):
+    #     for j in range(columnLenB):
+    #         for k in range(rowLenA):
+    #             result[i][j] += sampleMatrix2[k][i] * sampleMatrix1[k][j]
+    x = np.array(sampleMatrix1)
+    y = np.array(sampleMatrix2)
+    z = x @ y
+    result = np.array(z)
+    np.reshape(result, (3, 3))
     print(result)
 
 
 def rowProduct(sampleMatrix1, sampleMatrix2):
-    for i in range(rowLenA):
-        for j in range(columnLenB):
-            total = 0
-            for ii in range(columnLenA):
-                total += sampleMatrix1[i][ii] * sampleMatrix2[ii][j]
-            result[i][j] = total
-    print(result)
-
+    # for i in range(rowLenA):
+    #     for j in range(columnLenB):
+    #         for k in range(columnLenA):
+    #             result[i][j] += sampleMatrix1[i][k] * sampleMatrix2[k][j]
+    # print(result)
+    x = np.array(sampleMatrix1)
+    y = np.array(sampleMatrix2)
+    z = np.matmul(x, y)
+    print(z)
 
 
 def outerProduct(sampleMatrix1, sampleMatrix2):
-    print('Dekhiba3')
+    x = np.array(sampleMatrix1)
+    y = np.array(sampleMatrix2)
+
+    z = [[0,0,0],
+        [0,0,0],
+        [0,0,0]]
+    for j in range(len(x)):
+        matrix_temp_1 = x[:, j:j + 1]
+        matrix_temp_2 = y[j:j + 1, :]
+        # print (matrix_temp_1)
+        # print(matrix_temp_2)
+        num = np.dot(matrix_temp_1, matrix_temp_2)
+        # print(num)
+        z += num
+
+    print(z)
 
 
 def blockProduct(sampleMatrix1, sampleMatrix2):
-    print('Dekhiba4')
+    x = np.array(sampleMatrix1)
+    y = np.array(sampleMatrix2)
+
 
 
 # Comparing the Column length of A with Row Length of B matrix
@@ -70,7 +100,7 @@ print("1. Dot Product\n"
       "2. Column Method\n"
       "3. Row Method\n"
       "4. Outer Product\n"
-      "4. Block Multiplication")
+      "5. Block Multiplication")
 optionNumber = int(input("Choose a value above from 1 to 5\n"))
 
 print(optionNumber)
